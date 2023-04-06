c657cdbaf07f7ec44683b9b5a6ad67131e593958
Ir4D <iradedova@gmail.com>
2023-04-06 20:15:46 +0300

    feat: add overlay and no-scroll to open burger

--- shelter/script.js
+++ shelter/script.js
@@ -4,16 +4,22 @@
 
 const burgerMenu = document.querySelector(".burger-menu");
 const navContainer = document.querySelector(".nav-container");
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   navContainer.classList.toggle("active");
+  overlay.classList.toggle("visible");
+  body.classList.toggle("no-scroll");
 });
 
 document.querySelectorAll(".nav-link").forEach((elem) =>
   elem.addEventListener("click", () => {
     burgerMenu.classList.remove("active");
     navContainer.classList.remove("active");
+    overlay.classList.remove("visible");
+    body.classList.remove("no-scroll");
   })
 );
 
@@ -21,5 +27,7 @@ document.addEventListener('click', function(event) {
   if (!burgerMenu.contains(event.target)) {
     burgerMenu.classList.remove("active");
     navContainer.classList.remove("active");
+    overlay.classList.remove("visible");
+    body.classList.remove("no-scroll");
   }
 });
